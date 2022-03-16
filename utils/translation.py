1dbcaaf23a7533bf5dbf917b64ce72f2776108ad
labadier <rlabadiet@gmail.com>
2022-03-15 22:17:24 -0400

    fixed percent issue

--- utils/translation.py
+++ utils/translation.py
@@ -7,9 +7,10 @@ columns = ['campain', 'lang', 'tweet','sexism','ideological','stereotype','objec
 
 data_frame = pd.read_csv(os.path.join(root, 'data/augmented.csv'))
 
+print(len(data_frame))
 for target_lang in ['en', 'es', 'fr', 'de']:
 
-  print(f'Pivot Language {target_lang}')
+  print(f'Pivot Language {target_lang}: 0%', end="")
 
   perc = 0
   with open(os.path.join(root, f'data/{target_lang}.csv'), 'wt', newline='', encoding="utf-8") as csvfile:
@@ -17,13 +18,16 @@ for target_lang in ['en', 'es', 'fr', 'de']:
     spamwriter.writerow(columns)
     
     for i, row in enumerate(data_frame.iterrows()):
-      if perc - (i*1.0/2.0*len(data_frame)) > 0.01:
-        perc = (i*1.0/2.0*len(data_frame))
-        print(f'\rPivot Language {target_lang}: {perc*100.0:.2f}%', end = "")
+      if (i*50.0)/len(data_frame) - perc > 1:
+        perc = (i*50.0)/len(data_frame)
+      print(f'\rPivot Language {target_lang}: {(i*50.0)/len(data_frame):.2f}%', end = "")
 
       new_row = row[1].copy()
       if new_row[1] != target_lang:
-        new_row[2] = ts.google(new_row[2], from_language=new_row[1], to_language=target_lang)
+        try:
+          new_row[2] = ts.google(new_row[2], from_language=new_row[1], to_language=target_lang)
+        except:
+          print(f'An exception occurred {new_row[2]}')
       spamwriter.writerow(new_row)
 
   for back_target in ['en', 'es']:
@@ -35,13 +39,16 @@ for target_lang in ['en', 'es', 'fr', 'de']:
       spamwriter.writerow(columns)
       
       for row in data_frame.iterrows():
-        if perc - 50.0 + (i*1.0/2.0*len(data_frame)) > 0.01:
-          perc = 50.0 + (i*1.0/2.0*len(data_frame))
-          print(f'\rPivot Language {target_lang}: {perc*100.0:.2f}%', end = "")
+        if (i*50.0/len(data_frame)) - perc > 51.0:
+          perc = 50.0 + (i*50.0/len(data_frame))
+          print(f'\rPivot Language {target_lang}: {perc:.2f}%', end = "")
 
         new_row = row[1].copy()
-        new_row[2] = ts.google(new_row[2], from_language=target_lang, to_language=back_target)
+        try:
+          new_row[2] = ts.google(new_row[2], from_language=new_row[1], to_language=target_lang)
+        except:
+          print(f'An exception occurred: {new_row[2]}')
         spamwriter.writerow(new_row)
   print()
 
-# %%
+ # %%
