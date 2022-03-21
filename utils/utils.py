8a333d34c2436b4a4691d97ef147a6d2e1281310
labadier <rlabadiet@gmail.com>
2022-03-21 00:26:25 -0400

    data fixed

--- utils/utils.py
+++ utils/utils.py
@@ -5,11 +5,11 @@ from matplotlib import pyplot as plt
 def load_data(filename):
 
   data = pd.read_csv(filename, dtype=str)
-  text = data['tweet'].to_numpy()[:16]
+  text = data['tweet'].to_numpy()
     
   if len(data.keys()) < 4:
     return text
-  labels = data[params.columns[3:]].astype(int).to_numpy()[:16]
+  labels = data[params.columns[3:]].astype(int).to_numpy()
   return text, labels
 
 def evalData(filename, lang):
